a2bd8a25522e8f605266a27c4feb9564040d19b2
jungwookim <kjw0323@gmail.com>
2025-12-08 13:29:37 +0900

    aoc 2025-day06-part2

--- 2025/day06/main.go
+++ 2025/day06/main.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-func parse() [][]string {
+func parse1() [][]string {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
@@ -34,7 +34,7 @@ func parse() [][]string {
 }
 
 func part1() int {
-	lines := parse()
+	lines := parse1()
 	operators := lines[len(lines)-1]
 	results := make([]int, len(operators))
 	for i, op := range operators {
@@ -63,6 +63,104 @@ func part1() int {
 	return ans
 }
 
+func parse2() []string {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return lines
+}
+
+func skip(cols []string) bool {
+	for _, col := range cols {
+		if col != " " {
+			return false
+		}
+	}
+	return true
+}
+
+func colsToInt(cols []string) (int, string) {
+	var sb strings.Builder
+	for _, col := range cols {
+		if col == "+" {
+			n, _ := strconv.Atoi(sb.String())
+			return n, "+"
+		}
+		if col == "*" {
+			n, _ := strconv.Atoi(sb.String())
+			return n, "*"
+		}
+		if col != " " {
+			sb.WriteString(col)
+		}
+	}
+	n, _ := strconv.Atoi(sb.String())
+	return n, ""
+}
+
+func mul(numbers []int) int {
+	result := 1
+	for i := 0; i < len(numbers); i++ {
+		result *= numbers[i]
+	}
+	return result
+}
+
+func sum(numbers []int) int {
+	result := 0
+	for i := 0; i < len(numbers); i++ {
+		result += numbers[i]
+	}
+	return result
+}
+
+func part2() int {
+	lines := parse2()
+	ans := 0
+	var ps []int
+
+	for i := len(lines[0]) - 1; i >= 0; i-- {
+		var cols []string
+		for j := 0; j < len(lines); j++ {
+			cols = append(cols, string(lines[j][i]))
+		}
+		if skip(cols) {
+			ps = ps[:0]
+		} else {
+			n, op := colsToInt(cols)
+			if op == "" {
+				ps = append(ps, n)
+			} else {
+				ps = append(ps, n)
+				if op == "*" {
+					ans += mul(ps)
+				} else {
+					ans += sum(ps)
+				}
+			}
+
+		}
+	}
+
+	return ans
+}
+
 func main() {
 	fmt.Println(part1())
+	fmt.Println(part2())
 }
